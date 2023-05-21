7f849fed9d3f1f89c74bc32f711be13d64179bc0
Albina <albinaa1122333@gmail.com>
2023-05-22 01:25:05 +0300

    Win function

--- lab/lab_1.py
+++ lab/lab_1.py
@@ -14,3 +14,11 @@ spock = emoji.emojize(":vulcan_salute:")
 items = {1: "rock", 2: "paper", 3: "scissors", 4: "lizard", 5: "spock"}
 emoji_items = {1: rock, 2: paper, 3: scissors, 4: lizard, 5: spock}
 i = 0
+
+def is_win(user_item,bot_item):
+
+    if (user_item == 1 and bot_item in (3, 4)) or (user_item == 2 and bot_item in (1, 5)) or \
+            (user_item == 3 and bot_item in (2, 4)) or (user_item == 4 and bot_item in (2, 5)) \
+            or (user_item == 5 and bot_item in (1, 3)):
+        return True
+    return False
