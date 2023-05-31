e10e7007cedaea61772e27a0ffe68e78abc7175e
Albina <albinaa1122333@gmail.com>
2023-06-01 01:58:44 +0300

    Tests added and updated

--- test/test_lose.py
+++ test/test_lose.py
@@ -12,3 +12,17 @@ def choise():
 def test_choise(mock_input):
     assert choise() == '1'
     assert mock_input.called
+
+def test_lose_message():
+    assert lose() == "YOU LOSE!"
+
+def test_is_win():
+    assert is_win(1, 3) == True
+    assert is_win(4, 1) == False
+
+def test_is_win_draw():
+    assert is_win(3, 3) == False
+
+def test_win_mes():
+    assert win_mes(1, 3) == "You WIN!"
+    assert win_mes(5, 4) == "You LOSE!"
