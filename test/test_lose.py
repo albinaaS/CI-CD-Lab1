11de0c1d1830fffccc6976ee589bdf37292bd662
Albina <albinaa1122333@gmail.com>
2023-06-01 02:00:20 +0300

    Tests updated

--- test/test_lose.py
+++ test/test_lose.py
@@ -26,3 +26,15 @@ def test_is_win_draw():
 def test_win_mes():
     assert win_mes(1, 3) == "You WIN!"
     assert win_mes(5, 4) == "You LOSE!"
+
+def test_win_mes_player():
+    assert win_mes(2, 1, "Player") == "WIN of Player 1"
+    assert win_mes(2, 3, "Player") == "WIN of Player 2"
+
+@pytest.mark.skip(reason="I just wanna skip it :)")
+def test_win_mes_player_skip():
+    assert win_mes(3, 3, "Player") == "WIN of Player 1"
+
+@pytest.mark.skipif(sys.version_info > (3,7),reason="python more 3.7")
+def test_win_mes_player_skipif():
+    assert win_mes(3, 3, "Player") == "WIN of Player 1"
